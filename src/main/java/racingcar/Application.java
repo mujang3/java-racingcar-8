13a015df44ece5e50d2c0d3313997d9a693cc80b
carpbreadj <carpbreadj@gmail.com>
2025-10-26 22:20:56 +0900

    refactor(app): remove unnecessary try-catch for natural exception handling

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,7 +4,6 @@ import racingcar.controller.GameController;
 
 public class Application {
     public static void main(String[] args) {
-        try { new GameController().run(); }
-        catch (IllegalArgumentException e) { /* 조용히 종료 */ }
+        new GameController().run();
     }
 }
